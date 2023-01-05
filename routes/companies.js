5a26c5c71d701f6b2581bae72ebf9c731e0f57c8
michaelpappas <pappas.michael@gmail.com>
2023-01-05 12:32:30 -0800

    get and post routes working

---
+++ routes/companies.js
@@ -0,0 +1,80 @@
+"use strict"
+
+const express = require("express");
+
+const {NotFoundError} = require("../expressError")
+
+const db = require("../db");
+const router = new express.Router();
+
+/** Returns list of companies, like {companies: [{code, name}, ...]} */
+router.get("/companies", async function (req, res){
+  const results = await db.query(
+    `SELECT code, name
+      FROM companies`);
+    const companies = results.rows;
+    return res.json({companies});
+  });
+
+  /** Return obj of company: {company: {code, name, description}}
+
+      If the company given cannot be found, this should return a 404 status response.*/
+router.get("/companies/:code", async function (req, res){
+  const code = req.params.code
+  const results = await db.query(
+    `SELECT code, name, description
+      FROM companies
+      WHERE code = $1`, [code]
+  )
+  const company = results.rows[0]
+  if(company){
+    return res.json({ company })
+  }
+  else{
+    throw new NotFoundError()
+  }
+})
+
+
+/** Adds a company.
+
+      Needs to be given JSON like: {code, name, description}
+
+      Returns obj of new company: {company: {code, name, description}} */
+router.post("/companies", async function (req, res){
+  if (req.body === undefined) throw new BadRequestError();
+  const {code, name, description} = req.body;
+  const results = await db.query(
+    `INSERT INTO companies (code, name, description)
+      VALUES ($1, $2, $3)
+      RETURNING code, name, description`, [code, name, description]
+  )
+  const company = results.rows[0]
+  return res.status(201).json({ company });
+})
+
+/**Edit existing company.
+
+    Should return 404 if company cannot be found.
+
+    Needs to be given JSON like: {name, description}
+
+    Returns update company object: {company: {code, name, description}} */
+router.post("/companies", async function (req, res){
+  debugger;
+  if (req.body === undefined) throw new BadRequestError();
+  const {code, name, description} = req.body;
+  const result = await db.query(
+    `UPDATE companies
+           SET code=$1,
+               name=$2
+               description=$3
+           WHERE code = $1
+           RETURNING code, name, description`,
+    [code, name, description],
+  );
+  const company = results.rows[0]
+  return res.status(201).json({ company });
+})
+
+module.exports = router;
